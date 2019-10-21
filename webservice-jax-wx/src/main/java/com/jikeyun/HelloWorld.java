a8e2c11825ed4cf720b7fe240535736083d7b39d
dunfeng <991198121@qq.com>
2019-10-21 21:30:25 +0800

    读取文件

--- webservice-jax-wx/src/main/java/com/jikeyun/HelloWorld.java
+++ webservice-jax-wx/src/main/java/com/jikeyun/HelloWorld.java
@@ -1,20 +1,32 @@
 package com.jikeyun;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * @author 敦峰
  * @date 2019\10\21 0021 11:57
  */
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int a = 10;
         int b = 20;
-        System.out.println("a+b = " +(a+b));
+        System.out.println("a+b = " + (a + b));
         System.out.println("周末快乐!");
         System.out.println("周末快乐!");
         System.out.println("周末快乐!");
         System.out.println("周末快乐!");
         System.out.println("周末快乐!");
-     //添加变量
-        int c =5;
+        //添加变量
+        int c = 5;
+        FileInputStream fileInputStream = new FileInputStream("E:\\Workspace\\webservice\\webservice-jax-wx\\src\\main\\java\\com\\jikeyun\\a.txt");
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        byte [] bytes=new byte[1024];
+        int len;
+        while((len=bufferedInputStream.read(bytes))!=-1){
+            System.out.println(new String(bytes,0,len));
+        }
     }
 }
