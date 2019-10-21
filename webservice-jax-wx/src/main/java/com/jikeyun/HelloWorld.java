29616e16d908f95ab922fe8908445ed8c415ba19
dunfeng <991198121@qq.com>
2019-10-21 11:59:54 +0800

    webservice 尝试第一次提交!

---
+++ webservice-jax-wx/src/main/java/com/jikeyun/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.jikeyun;
+
+/**
+ * @author 敦峰
+ * @date 2019\10\21 0021 11:57
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("周末快乐!");
+        System.out.println("周末快乐!");
+        System.out.println("周末快乐!");
+        System.out.println("周末快乐!");
+        System.out.println("周末快乐!");
+
+    }
+}
